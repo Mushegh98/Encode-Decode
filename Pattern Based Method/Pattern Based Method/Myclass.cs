55e1fe8ec5269ebed0c59874276606dddf8119ea
Mushegh98 <53486603+Mushegh98@users.noreply.github.com>
2019-10-21 00:07:36 +0400

    Add files via upload

---
+++ Pattern Based Method/Pattern Based Method/Myclass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pattern_Based_Method
+{
+    static class Myclass
+    {
+        
+        public static string Encoding(string text)
+        {
+            string ReturnedText = "";
+            string EncodeText = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmopqrstuvwxyz";
+              string SwapText = "0123456789abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            for(int i=0;i<text.Length;i++)
+            {
+                int index = EncodeText.IndexOf(text[i]);
+                if(index==-1)
+                {
+                    ReturnedText += text[i];
+                }
+
+                else
+                {
+                    ReturnedText += SwapText[index];
+                }
+            }
+            return ReturnedText;
+        }
+        public static string Decoding(string text)
+        {
+            string ReturnedText = "";
+              string SwapText = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmopqrstuvwxyz";
+            string DecodeText = "0123456789abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            for (int i = 0; i < text.Length; i++)
+            {
+                int index = DecodeText.IndexOf(text[i]);
+                if (index == -1)
+                {
+                    ReturnedText += text[i];
+                }
+
+                else
+                {
+                    ReturnedText += SwapText[index];
+                }
+            }
+            return ReturnedText;
+        }
+
+    }
+}
---
+++ Pattern Based Method/Pattern Based Method/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Pattern_Based_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string EncodeText = Myclass.Encoding("Hello world!");
+            Console.WriteLine($"Encode Text->{EncodeText}");
+            string DecodeText = Myclass.Decoding(EncodeText);
+            Console.WriteLine(new string('-', 25));
+            Console.WriteLine($"Decode Text->{DecodeText}");
+        }
+    }
+}
